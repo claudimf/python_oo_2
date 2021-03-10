914dc6b50f999c26666c63a90ed82a28adffb62b
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-10 11:47:49 -0300

    Makes course class "Adicionando atributos e métodos"

--- src/aulas/01.py
+++ src/aulas/01.py
@@ -1,15 +1,23 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -17,3 +25,15 @@ print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores
 
 atlanta = Serie('atlanta', 2018, 2)
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
+f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+atlanta = Serie('atlanta', 2018, 2)
+atlanta.dar_like()
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
+f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
