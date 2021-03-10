46cae1f58a67f5005f2675eb0f138dc552afe958
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-10 12:44:58 -0300

    Makes course class "Encapsulando comportamento"

--- src/aulas/01.py
+++ src/aulas/01.py
@@ -1,12 +1,24 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
 
 
 class Serie:
@@ -14,10 +26,22 @@ class Serie:
         self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
